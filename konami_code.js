16eb298b74634125ec4cd3964fb645fd4f341ddd
cheerful-response-5821 <bryan.nguyen2494@gmail.com>
2020-05-23 22:02:32 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -21,13 +21,13 @@ function init() {
 
 
 let index = 0;
- 
+
 function cheatCodes(codes) {
   const key = codes.key;
- 
+
   if (key === codes[index]) {
     index++;
- 
+
     if (index === codes.length) {
       alert("Hurray!");
     }
