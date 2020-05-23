9ddc7948d9a971a5965781c6b56239a2575da41a
cheerful-response-5821 <bryan.nguyen2494@gmail.com>
2020-05-23 22:02:29 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -16,3 +16,22 @@ function init() {
       alert('Hurray');
   })
 }
+
+
+
+
+let index = 0;
+ 
+function cheatCodes(codes) {
+  const key = codes.key;
+ 
+  if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Hurray!");
+    }
+  } else {
+    index = 0;
+  }
+}
